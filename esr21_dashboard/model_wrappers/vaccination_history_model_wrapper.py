2c28fbb6162bdccfb877dfd11bd11fbce12b9774
ame diphoko <diphokoame@gmail.com>
2022-08-16 14:18:56 +0200

    prefill vaccination history form, with vax details

--- esr21_dashboard/model_wrappers/vaccination_history_model_wrapper.py
+++ esr21_dashboard/model_wrappers/vaccination_history_model_wrapper.py
@@ -8,4 +8,13 @@ class VaccinationHistoryModelWrapper(ModelWrapper):
     next_url_attrs = ['subject_identifier']
     next_url_name = settings.DASHBOARD_URL_NAMES.get(
         'screening_listboard_url')
-    querystring_attrs = ['subject_identifier']
+
+    @property
+    def querystring_attrs(self):
+        options = ['subject_identifier', ]
+        prefil_fields = ['received_vaccine', 'dose_quantity',
+                         'dose1_product_name', 'dose1_date',
+                         'dose2_product_name', 'dose2_date',
+                         'dose3_product_name', 'dose3_date', ]
+        options.extend(prefil_fields)
+        return options
